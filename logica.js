ca518ad2e55f53aaa5ce94813e8f963d4a4b4b68
Camilo Betancur <betancurcamilo50@gmail.com>
2022-08-16 20:25:44 -0500

    Se hizo la logica de la aplicacion web

---
+++ logica.js
@@ -0,0 +1,30 @@
+let x = 1;
+while (x == 1) {
+  let nombre = prompt("Ingrese su nombre: ");
+  let apellido = prompt("Ingrese su apellido: ");
+  let correo = prompt("Ingrese su correo electronico: ");
+  let monto_prestamo = parseInt(prompt("Ingrese el monto del prestamo:"));
+  let interes_mensual = parseFloat(
+    prompt("Ingrese el porcentaje de interes mensual en cifras decimales: ")
+  );
+  let meses = parseInt(
+    prompt("Ingrese la cantidad de meses en los que desea pagar el prestamo: ")
+  );
+
+  let pago_mensual =
+    (interes_mensual * monto_prestamo) / (1 - (1 + interes_mensual) ** -meses);
+
+  console.log(
+    "El/la señor@  ",
+    nombre,
+    " ",
+    apellido,
+    "pagara mensualmente:",
+    pago_mensual,
+    " pesos"
+  );
+
+  x = parseInt(
+    prompt("Ingrese 1 si quieres volver a realizar otra analisis de prestamo: ")
+  );
+}
