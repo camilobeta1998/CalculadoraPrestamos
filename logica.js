524bd0ae19ef99b94f9d986d627e2507dcb58bf3
Camilo Betancur <betancurcamilo50@gmail.com>
2022-08-25 20:47:15 -0500

    Se uso el metodo filter para buscar en el array el nombre

--- logica.js
+++ logica.js
@@ -19,6 +19,7 @@ class Usuario {
 }
 
 function calcular_prestamo() {
+  //declaro x para saber si quiero volver a realizar el ciclo
   let x = 1;
   let array = [];
   while (x == 1) {
@@ -35,6 +36,7 @@ function calcular_prestamo() {
       )
     );
 
+    //aqui calculo el pago mensual
     let pago_mensual =
       (interes_mensual * monto_prestamo) /
       (1 - (1 + interes_mensual) ** -meses);
@@ -48,7 +50,7 @@ function calcular_prestamo() {
       pago_mensual,
       " pesos"
     );
-
+    //creo el objeto usuario usando la clase Usuario
     let usuario = new Usuario(
       nombre,
       apellido,
@@ -59,15 +61,44 @@ function calcular_prestamo() {
       pago_mensual
     );
 
+    //estoy pusheando los objetos al array
     array.push(usuario);
-
     x = parseInt(
       prompt(
-        "Ingrese 1 si quieres volver a realizar otra analisis de prestamo: "
+        "Ingrese 1 si quieres volver a realizar otro analisis de prestamo o ingrese un numero diferente si quiero salir: "
       )
     );
   }
   console.log(array);
+  console.log(
+    "---------------------------------------------------------------------------------"
+  );
+
+  // y es la variable para saber si el ciclo se continua repitiendo
+  let y = 1;
+  while (y == 1) {
+    let nombre_busqueda = prompt(
+      "Ingrese el nombre que quiere buscar dentro de la base de datos: "
+    );
+    let busqueda = array.filter(function (usuario) {
+      if (usuario.nombre == nombre_busqueda) {
+        return true;
+      } else {
+        console.log("Este usuario NO esta en la base de datos");
+      }
+    });
+    console.log(
+      "Los valores obtenidos para:  ",
+      nombre_busqueda,
+      " son: ",
+      busqueda
+    );
+    y = parseInt(
+      prompt(
+        "Ingrese el numero 1 si  quiere volver a buscar a alguien en la base de datos, ingrese otro numero si quiere salir: "
+      )
+    );
+  }
 }
 
 calcular_prestamo();
